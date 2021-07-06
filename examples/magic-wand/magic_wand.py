6e1330ee31ac44f0d71df993a634dd6db6a9e5c2
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2021-07-06 10:45:16 +0200

    Add navigation images to notebooks

--- examples/magic-wand/magic_wand.py
+++ examples/magic-wand/magic_wand.py
@@ -3,13 +3,13 @@
 ![Renode](https://dl.antmicro.com/projects/renode/renode.png)
 <table align="left">
   <td>
-    <a target="_blank" href="https://colab.research.google.com/github/antmicro/tensorflow-zephyr-vexriscv-examples/blob/main/examples/magic-wand/$NOTEBOOK.ipynb"><img src="https://www.tensorflow.org/images/colab_logo_32px.png" />Run in Google Colab</a>
+    <a target="_blank" href="https://colab.research.google.com/github/antmicro/tensorflow-zephyr-vexriscv-examples/blob/main/examples/magic-wand/$NOTEBOOK.ipynb"><img src="https://raw.githubusercontent.com/antmicro/tensorflow-zephyr-vexriscv-examples/master/examples/.static/view-in-colab.png" />Run in Google Colab</a>
   </td>
   <td>
-    <a target="_blank" href="https://github.com/antmicro/tensorflow-zephyr-vexriscv-examples/blob/main/examples/magic-wand/$NOTEBOOK.ipynb"><img src="https://www.tensorflow.org/images/GitHub-Mark-32px.png" />View ipynb on GitHub</a>
+    <a target="_blank" href="https://github.com/antmicro/tensorflow-zephyr-vexriscv-examples/blob/main/examples/magic-wand/$NOTEBOOK.ipynb"><img src="https://raw.githubusercontent.com/antmicro/tensorflow-zephyr-vexriscv-examples/master/examples/.static/view-ipynb.png" />View ipynb on GitHub</a>
   </td>
   <td>
-    <a target="_blank" href="https://github.com/antmicro/tensorflow-zephyr-vexriscv-examples/blob/main/examples/magic-wand/magic_wand.py"><img src="https://www.tensorflow.org/images/GitHub-Mark-32px.png" />View Python source on GitHub</a>
+    <a target="_blank" href="https://github.com/antmicro/tensorflow-zephyr-vexriscv-examples/blob/main/examples/magic-wand/magic_wand.py"><img src="https://raw.githubusercontent.com/antmicro/tensorflow-zephyr-vexriscv-examples/master/examples/.static/view-source.png" />View Python source on GitHub</a>
   </td>
 </table>
 """
