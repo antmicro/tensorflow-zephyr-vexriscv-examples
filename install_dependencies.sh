25fe9b3f8dd39c6854410e422ef33d75bf4a3978
Jakub Jatczak <jjatczak@antmicro.com>
2021-08-16 09:32:48 +0200

    Add PIL libary to dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,6 @@
 sudo apt update && sudo apt install -y cmake ninja-build gperf ccache dfu-util device-tree-compiler wget python python3-pip python3-setuptools python3-tk python3-wheel xz-utils file make gcc gcc-multilib locales tar curl unzip xxd
 pip install --upgrade cmake virtualenv
+pip install pillow
 wget https://github.com/zephyrproject-rtos/sdk-ng/releases/download/v0.11.2/zephyr-sdk-0.11.2-setup.run
 chmod +x zephyr-sdk-0.11.2-setup.run
 ./zephyr-sdk-0.11.2-setup.run -- -y -d $ZEPHYR_SDK_INSTALL_DIR
