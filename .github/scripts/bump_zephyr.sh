0ee7905db174d380d0e423140efad4adc7f0d223
Jakub Jatczak <jjatczak@antmicro.com>
2021-08-20 10:40:43 +0200

    Fix zephyr-sdk version selection
    The reason of failures was that the sdk version was to low, this commit changes the approach of downloading it. Now it always checks for latest version.

--- .github/scripts/bump_zephyr.sh
+++ .github/scripts/bump_zephyr.sh
@@ -13,10 +13,19 @@ BUMP_ZEPHYR_TF_MD5="ZEPHYR_MD5 := \"${BUMP_ZEPHYR_MD5}\""
 sed -i "s@^ZEPHYR_URL :=.*@${BUMP_ZEPHYR_TF_URL}@" tflite-micro/tensorflow/lite/micro/tools/make/third_party_downloads.inc
 sed -i "s@^ZEPHYR_MD5 :=.*@${BUMP_ZEPHYR_TF_MD5}@" tflite-micro/tensorflow/lite/micro/tools/make/third_party_downloads.inc
 
-ZEPHYR_SDK_RELEASES_URL="https://github.com/zephyrproject-rtos/sdk-ng/releases/download"
-ZEPHYR_SDK_VERSION="v0.12.4"
-ZEPHYR_SDK_FILENAME="zephyr-sdk-0.12.4-x86_64-linux-setup.run"
+# Finding latest release of the zephyr-sdk. See https://docs.github.com/en/rest/reference/repos#releases for details
+set +x
+RELEASES_JSON=`curl https://api.github.com/repos/zephyrproject-rtos/sdk-ng/releases 2>/dev/null`
+SDK_VERSION=`echo "${RELEASES_JSON}" |grep 'tag_name' | head --lines 1 | grep --extended-regexp --only-matching '[0-9]+\.[0-9]+\.[0-9]+'`
+if [ -z $SDK_VERSION]; then
+    echo Failed to get latest SDK version && exit 1
+fi;
 
-sed -i "s@^wget ${ZEPHYR_SDK_RELEASES_URL}.*@wget ${ZEPHYR_SDK_RELEASES_URL}/${ZEPHYR_SDK_VERSION}/${ZEPHYR_SDK_FILENAME}@" install_dependencies.sh
+set -x
+
+ZEPHYR_SDK_RELEASES_URL="https://github.com/zephyrproject-rtos/sdk-ng/releases/download"
+ZEPHYR_SDK_FILENAME="zephyr-sdk-${SDK_VERSION}-x86_64-linux-setup.run"
+
+sed -i "s@^wget ${ZEPHYR_SDK_RELEASES_URL}.*@wget ${ZEPHYR_SDK_RELEASES_URL}/v${SDK_VERSION}/${ZEPHYR_SDK_FILENAME}@" install_dependencies.sh
 sed -i "s@^chmod +x.*@chmod +x ${ZEPHYR_SDK_FILENAME}@" install_dependencies.sh
 sed -i "s@^\./.*@./${ZEPHYR_SDK_FILENAME} -- -y -d \$ZEPHYR_SDK_INSTALL_DIR@" install_dependencies.sh
